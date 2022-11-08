2b93a7a5a98f5bdd8bea578b3ac198a0a6ca8319
rohan <panditrohan738@gmail.com>
2022-11-08 19:24:26 +0530

    reson

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ const checkWin = ()=>
 }
 
 // Game logic
-// music.play();
+music.play();
 let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(element =>{
     let boxtext= element.querySelector('.boxtext');
